7d6b7e1240d3c8a29c76a0a69632fc6ef59056a7
Alex Alegre <alexander.alegre@outlook.com>
2019-02-17 21:38:00 -0700

    create phaser 3 basic template

---
+++ js/sceneMain.js
@@ -0,0 +1,15 @@
+class SceneMain extends Phaser.Scene {
+  constructor() {
+      super('SceneMain');
+  }
+
+  preload() {
+    
+  }
+
+  create() {
+      console.log("Ready!");
+  }
+
+  update() {}
+}
